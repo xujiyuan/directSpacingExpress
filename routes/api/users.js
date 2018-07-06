6cee6ca53cfd9a78aa0128a4cc1cdd784cbdc06a
Jiyuan Xu <xuj1@ohio.edu>
2018-07-05 22:15:32 -0400

    add passport js for authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -44,15 +44,38 @@ router.put('/user/register', auth.optional, async (req, res) => {
     }
 });
 
-router.put('/user/login', auth.optional, async (req, res) => {
-    try {
-        let result = await UserService.validate(req.body);
-        res.status(200).json({authToken: result});
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err.message);
+// router.put('/user/login', auth.optional, async (req, res) => {
+//     try {
+//         let result = await UserService.validate(req.body);
+//         res.status(200).json({authToken: result});
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json(err.message);
+//     }
+// });
+
+router.post('/user/login', function(req, res, next){
+    if(!req.body.user.email){
+        return res.status(422).json({errors: {email: "can't be blank"}});
     }
+
+    if(!req.body.user.password){
+        return res.status(422).json({errors: {password: "can't be blank"}});
+    }
+
+    passport.authenticate('local', {session: false}, function(err, user, info){
+        if(err){ return next(err); }
+
+        if(user){
+            user.token = user.generateJWT();
+            return res.json({user: user.toAuthJSON()});
+        } else {
+            return res.status(422).json(info);
+        }
+    })(req, res, next);
 });
+
+
 
 // router.post('/users/login', function (req, res, next) {
 //     if (!req.body.user.email) {
